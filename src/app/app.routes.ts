6208840da28abacfc84de4fd5ded4ebe85ce636d
Daniela Rodriguez <daniela.rodriguezf5@gmail.com>
2025-02-28 17:20:27 -0500

    list component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './pages/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'list',
+        component: ListComponent
+    },
+    {
+        path: '',
+        redirectTo: 'list',
+        pathMatch: 'full'
+    }
+];
