873553e9d5cc7a3e14e7373f25cabd2168073156
Daniela Rodriguez <daniela.rodriguezf5@gmail.com>
2025-03-01 10:24:12 -0500

    Add alert when an error occurs

--- src/app/pages/business/detail/detail.component.ts
+++ src/app/pages/business/detail/detail.component.ts
@@ -1,18 +1,20 @@
 import { Component, inject, Input } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { DetailService } from '../../../services/detail.service';
 import { Subscription, take } from 'rxjs';
 import { Detail } from '../../../core/detail.interface';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-detail',
   imports: [RouterModule],
   templateUrl: './detail.component.html',
-  styleUrl: './detail.component.css'
+  styleUrl: './detail.component.css',
 })
 export class DetailComponent {
   private readonly detailService: DetailService = inject(DetailService);
   private detailSubscription: Subscription = new Subscription();
+  private readonly router = inject(Router);
   detailsData!: Detail;
 
   @Input() id: string = '';
@@ -30,6 +32,15 @@ export class DetailComponent {
           this.detailsData = response.state;
         },
         error: (error) => {
+          Swal.fire({
+            title: 'Oops...',
+            text: `Ocurrió un error. No se pudo obtener los detalles de ${this.id}`,
+            icon: 'error',
+            confirmButtonText: 'Continuar',
+            timer: 6000,
+          }).then(() => {
+            this.router.navigate(['/menu/list']);
+          });
           console.error('Error al mostrar los detalles', error);
         },
       });
