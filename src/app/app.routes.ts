eb30646905d59bcfad5240652f81fadf15bb4363
DanielaRodriguez0 <78557823+DanielaRodriguez0@users.noreply.github.com>
2025-02-28 21:38:37 -0500

    List and see detail components (#2)
    * list component
    
    * feat: detail and list components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './pages/list/list.component';
+import { DetailsComponent } from './pages/details/details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'list',
+        component: ListComponent
+    },
+    {
+        path: 'list/:id/details',
+        component: DetailsComponent
+    },
+    {
+        path: '',
+        redirectTo: 'list',
+        pathMatch: 'full'
+    }
+];
---
+++ src/app/pages/list/list.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+export interface PeriodicElement {
+  name: string;
+  id: string;
+  weight: number;
+  symbol: string;
+}
+
+@Component({
+  selector: 'app-list',
+  imports: [RouterModule],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent implements OnInit {
+
+  ELEMENT_DATA: PeriodicElement[] = [
+    {id: '559dbfaf-bc67-44ce-a880-a754b5ba2255', name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+    {id: 'asdfasdf-wer-asdfa', name: 'Helium', weight: 4.0026, symbol: 'He'},
+    {id: 'adfdf-w45era-asdf-asd', name: 'Lithium', weight: 6.941, symbol: 'Li'},
+    {id: 'adf-adfdsgf-aretfa', name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+    {id: 'sdf-a4df-wrtg-adf', name: 'Boron', weight: 10.811, symbol: 'B'},
+    {id: 'asd-adf-atqqasdf-asa', name: 'Carbon', weight: 12.0107, symbol: 'C'},
+    {id: 'asdf-asdf-adf7', name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+    {id: 'adf5-adf-34a8', name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+    {id: 'adf-35azd-adfa-as22', name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+    {id: '1aadsf-45a-aadf0', name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  ];
+
+  ngOnInit(): void {
+    console.log('Element: ', this.ELEMENT_DATA );
+  }
+
+
+}
